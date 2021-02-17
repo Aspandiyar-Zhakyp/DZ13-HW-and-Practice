d9a4a3ec1dc5453b83da4091a4ee4774dfb9b04b
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-02-17 11:12:50 +0600

    DZ / Delegate

---
+++ DelegateHW/DelegateHW/Button.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateHW
+{
+    public delegate void ButtonActionDelegate<T>(params T[] value);
+    public class Button
+    {
+        public char BorderSymbol { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Text { get; set; }
+        public ConsoleColor BackgroundColor { get; set; }
+        public ConsoleColor BorderColor { get; set; }
+        public ButtonActionDelegate<string> Action { get; set; }
+        public void ShowButton()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    if (j == 0 || i == 0 || j == Width - 1 || i == Height - 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.BackgroundColor = BorderColor;
+                        Console.Write(BorderSymbol);
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = BackgroundColor;
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.SetCursorPosition(Width / 5, Height / 3);
+            Console.Write(Text);
+            Console.ResetColor();
+            Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void PressTheButton()
+        {
+            Action?.Invoke("==(*)", "You wrote it seriously, you couldn't think of anything else", "(*)==");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DelegateHW/DelegateHW/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegateHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var button = new Button
+            {
+                BorderSymbol = '*',
+                Text = "Write something",
+                Width = 20,
+                Height = 10,
+                BackgroundColor = ConsoleColor.Black,
+                BorderColor = ConsoleColor.Yellow,
+                Action = new ButtonActionDelegate<string>(PrintStrings)
+            };
+            button.ShowButton();
+            button.PressTheButton();
+        }
+        private static void PrintStrings(string[] values)
+        {
+            foreach (var str in values)
+            {
+                Console.Write(str);
+            }
+        }
+    }
+}
